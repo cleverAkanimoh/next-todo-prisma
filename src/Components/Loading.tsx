f25821b9db7d532269ba30a954a1961815c5551b
= <=>
2023-07-02 19:57:50 +0100

    deleted Loading.tsx from Components

--- src/Components/Loading.tsx
+++
@@ -1,8 +0,0 @@
-import loadingGif from '../assets/images/mona-loading-dark.gif'
-
-export default () => (
-  <div className="loading-div-style">
-    <img src={loadingGif} alt="loading..." className='loading-img-style' />
-    <h2 className="loading-h2-style">loading projects...</h2>
-  </div>
-)
