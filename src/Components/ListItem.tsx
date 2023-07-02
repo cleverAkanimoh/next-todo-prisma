30db7491619c98002cb90756769684d0fea57329
= <=>
2023-07-02 16:41:43 +0100

    modified page.tsx added ListItem.tsx

---
+++ src/Components/ListItem.tsx
@@ -0,0 +1,9 @@
+type Props = {}
+
+const ListItem = (props: Props) => {
+  return (
+    <div>ListItem</div>
+  )
+}
+
+export default ListItem
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ export default function Home() {
   return (
     <main>
     
-      <header className="">
+      <header className="flex justify-between bg-yellow-50">
         <h1><span>Next</span> Todos</h1>
         <Link href='new'>New</Link>
       </header>
       
-      <ul></ul>
+      <ul>{
+      // todos.map(todos => <ListItem key={key} {...todos} />)
+      }</ul>
     
     </main>
   )
